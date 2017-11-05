43bf653b580bdd06aead547eb4f1332356b0ff00
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-11-05 10:46:22 -0600

    Working with Iris Framework

--- main.go
+++ main.go
@@ -2,43 +2,109 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/basicauth"
 	"github.com/kataras/iris/mvc"
 )
 
+type Movie struct {
+	Name string 	`json:"name"`
+	Year int 		`json:"year"`
+	Genre string	`json:"genre"`
+	Poster string	`json:"poster"`
+}
+
+var movies = []Movie{
+	{
+		Name: "Casablanca",
+		Year:   1942,
+		Genre:  "Romance",
+		Poster: "https://iris-go.com/images/examples/mvc-movies/1.jpg",
+	},
+	{
+		Name:   "Gone with the Wind",
+		Year:   1939,
+		Genre:  "Romance",
+		Poster: "https://iris-go.com/images/examples/mvc-movies/2.jpg",
+	},
+	{
+		Name:   "Citizen Kane",
+		Year:   1941,
+		Genre:  "Mystery",
+		Poster: "https://iris-go.com/images/examples/mvc-movies/3.jpg",
+	},
+	{
+		Name:   "The Wizard of Oz",
+		Year:   1939,
+		Genre:  "Fantasy",
+		Poster: "https://iris-go.com/images/examples/mvc-movies/4.jpg",
+	},
+}
+
+var basicAuth = basicauth.New(basicauth.Config{
+	Users: map[string]string{
+		"admin":"password",
+	},
+})
 
 func main(){
 	app := iris.New()
 
-	app.Controller("/helloWorld", new(HelloWorldController))
-
-	app.Run(iris.Addr(":8081"), iris.WithoutServerError(iris.ErrServerClosed))
+	app.Use(basicAuth)
+	app.Controller("/movies", new(MoviesController))
+	app.Run(iris.Addr(":8081"))
 }
 
-type HelloWorldController struct {
+
+type MoviesController struct {
 	mvc.C
-
-	// [ Your fields here ]
-	// Request lifecycle data
-	// Models
-	// Database
-	// Global properties
 }
 
-func (c *HelloWorldController) Get() string {
-	return "This is my default action... GET"
+
+// Get returns list of the movies
+// Demo:
+// curl -i http://localhost:8080/movies
+func (c *MoviesController) Get() []Movie{
+	return movies
 }
 
-func (c *HelloWorldController) GetBy(name string) string {
-	return "Hello " + name
+// curl -i http://localhost:8080/movies/1
+func (c *MoviesController) GetBy(id int) Movie{
+	return movies[id]
 }
 
-func (c *HelloWorldController) GetWelcome() (string, int) {
-	return "This is the GetWelcome action func...", iris.StatusOK
+// PutBy updates a movie
+// Demo:
+// curl -i -X PUT -F "genre=Thriller" -F "poster=@/Users/kataras/Downloads/out.gif" http://localhost:8080/movies/1
+func (c *MoviesController) PutBy(id int) Movie{
+
+	m := movies[id]
+	file, info, err := c.Ctx.FormFile("poster")
+
+	if err != nil{
+		c.Ctx.StatusCode(iris.StatusInternalServerError)
+		return Movie{}
+	}
+
+	file.Close()
+
+	poster := info.Filename
+	genre := c.Ctx.FormValue("genre")
+
+	// update the poster
+	m.Poster = poster
+	m.Genre = genre
+	movies[id] = m
+	return m
 }
 
-// GET: /helloworld/welcome/{name:string}/{numTimes:int}
-func (c *HelloWorldController) GetWelcomeBy(name string, numTimes int) {
-	// Access to the low-level Context,
-	// output arguments are optional of course so we don't have to use them here.
-	c.Ctx.Writef("Hello %s NumTimes is: %d", name, numTimes)
+
+// DeleteBy deletes a movie
+// Demo:
+// curl -i -X DELETE -u admin:password http://localhost:8080/movies/1
+func (c *MoviesController) DeleteBy(id int) iris.Map {
+	deleted := movies[id].Name
+
+	movies = append(movies[:id], movies[id+1:]...)
+	// and return the deleted movie's name
+	return iris.Map{"deleted": deleted}
 }
