05dc6f810f82698c2dc8ac2f2f8ea534f8cae626
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-11-05 01:13:36 -0600

    MVC Play

--- main.go
+++ main.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"github.com/kataras/iris"
-	//"github.com/kataras/iris/middleware/logger"
-	//"github.com/kataras/iris/middleware/recover"
+	"github.com/kataras/iris/mvc"
 )
 
 
 func main(){
 	app := iris.New()
 
-	app.RegisterView(iris.HTML("./views", ".html"))
-
-	app.Get("/", func(ctx iris.Context){
-
-		ctx.ViewData("message", "Hello World!")
-
-		ctx.View("hello.html")
-	})
-
-	app.Get("/user/{id:long}", func(ctx iris.Context){
-
-		userID, _ := ctx.Params().GetInt64("id")
-		ctx.Writef("User ID: %d", userID)
-
-	})
-
-	//app.Use(recover.New())
-	//app.Use(logger.New())
-
-
+	app.Controller("/helloWorld", new(HelloWorldController))
 
 	app.Run(iris.Addr(":8081"), iris.WithoutServerError(iris.ErrServerClosed))
+}
+
+type HelloWorldController struct {
+	mvc.C
+
+	// [ Your fields here ]
+	// Request lifecycle data
+	// Models
+	// Database
+	// Global properties
+}
+
+func (c *HelloWorldController) Get() string {
+	return "This is my default action... GET"
+}
+
+func (c *HelloWorldController) GetBy(name string) string {
+	return "Hello " + name
+}
+
+func (c *HelloWorldController) GetWelcome() (string, int) {
+	return "This is the GetWelcome action func...", iris.StatusOK
+}
+
+// GET: /helloworld/welcome/{name:string}/{numTimes:int}
+func (c *HelloWorldController) GetWelcomeBy(name string, numTimes int) {
+	// Access to the low-level Context,
+	// output arguments are optional of course so we don't have to use them here.
+	c.Ctx.Writef("Hello %s NumTimes is: %d", name, numTimes)
 }
