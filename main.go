c7493e5acf046810b9ef810510285e28359a90bb
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-11-05 01:00:27 -0600

    Playing with Views

--- main.go
+++ main.go
@@ -2,43 +2,34 @@ package main
 
 import (
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/logger"
-	"github.com/kataras/iris/middleware/recover"
+	//"github.com/kataras/iris/middleware/logger"
+	//"github.com/kataras/iris/middleware/recover"
 )
 
 
 func main(){
 	app := iris.New()
 
-	// Optionally, add two built'n handlers
-	// that can recover from any http-relative panics
-	// and log the requests to the terminal.
+	app.RegisterView(iris.HTML("./views", ".html"))
 
-	app.Use(recover.New())
-	app.Use(logger.New())
+	app.Get("/", func(ctx iris.Context){
 
-	// Method:   GET
-	// Resource: http://localhost:8080
-	app.Handle("GET", "/", func(ctx iris.Context){
-		ctx.HTML("<h1>Hi Katara Iris</h1>")
+		ctx.ViewData("message", "Hello World!")
+
+		ctx.View("hello.html")
 	})
 
-	// same as app.Handle("GET", "/ping", [...])
-	// Method:   GET
-	// Resource: http://localhost:8080/ping
-	app.Get("/ping", func(ctx iris.Context){
-		ctx.WriteString("pong")
+	app.Get("/user/{id:long}", func(ctx iris.Context){
+
+		userID, _ := ctx.Params().GetInt64("id")
+		ctx.Writef("User ID: %d", userID)
+
 	})
 
-	// Method:   GET
-	// Resource: http://localhost:8080/hello
-	app.Get("hello", func(ctx iris.Context){
-		ctx.JSON(iris.Map{"message":"Hello Iris World"})
-	})
+	//app.Use(recover.New())
+	//app.Use(logger.New())
 
-	// http://localhost:8080
-	// http://localhost:8080/ping
-	// http://localhost:8080/hello
+
 
 	app.Run(iris.Addr(":8081"), iris.WithoutServerError(iris.ErrServerClosed))
 }
